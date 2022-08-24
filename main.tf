5abbc52e2d6150bdd3e70aafde1de59169d560bf
Ram Narayan <ramucv@hotmail.com>
2022-08-24 21:58:08 +0000

    Added the second tag

--- main.tf
+++ main.tf
@@ -110,7 +110,10 @@ resource "azurerm_virtual_machine" "catapp" {
 
   network_interface_ids         = [azurerm_network_interface.catapp-nic.id]
   delete_os_disk_on_termination = "true"
-  tags = {Department = "devops"}
+  tags = {
+    Department = "devops"
+    Billable = "true"
+    }
 
   storage_image_reference {
     publisher = var.image_publisher
