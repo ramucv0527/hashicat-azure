9a132d7251dbcda97daaa151f0fbda4b8230e16e
Ram Narayan <ramucv@hotmail.com>
2022-08-25 00:27:00 +0000

    trying resouce name 1

--- network.tf
+++ network.tf
@@ -1,6 +1,6 @@
 module "network" {
   source  = "app.terraform.io/ramnarayan/network/azurerm"
   version = "3.5.0"
-  resource_group_name ="network"
+  resource_group_name ="ramucv-workshop"
   # insert required variables here
 }
