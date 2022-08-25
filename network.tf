6fdb3fb6475b1b7059aa18196b5cf0e79707c6c4
Ram Narayan <ramucv@hotmail.com>
2022-08-25 00:21:19 +0000

    Added resouce name

--- network.tf
+++ network.tf
@@ -1,6 +1,6 @@
 module "network" {
   source  = "app.terraform.io/ramnarayan/network/azurerm"
   version = "3.5.0"
-  resource_group_name ="gaurav"
+  resource_group_name ="network"
   # insert required variables here
 }
